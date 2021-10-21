eda3d564173282f90fe6ae8758b92184c30ba39c
AlyceOsbourne <39281408+AlyceOsbourne@users.noreply.github.com>
2021-10-21 11:53:09 +0100

    Working, now with multithreading
    Signed-off-by: AlyceOsbourne <39281408+AlyceOsbourne@users.noreply.github.com>

---
+++ src/main/java/application/SavaData.java
@@ -0,0 +1,10 @@
+package application;
+
+import application.data.Character;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SavaData {
+	Map<String, Character> savedCharacters = new HashMap<>();
+}
